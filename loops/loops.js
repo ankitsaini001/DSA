e079c0127fe21c017bc46c7caf7514f053ff3681
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-11 23:59:37 +0530

    loops01

--- loops/loops.js
+++ loops/loops.js
@@ -63,3 +63,10 @@ function greet(x) {
 for (let i = 0; i < 5; i++) { 
     greet(i);
 }
+
+// array with loop
+let arr = [10, 20, 30, 40, 50, 60];
+//arr.length , it calculate the length
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
