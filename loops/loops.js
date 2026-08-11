4bfb51048362677253bc1e48f8b70f413633c13d
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-11 23:27:05 +0530

    loops01

---
+++ loops/loops.js
@@ -0,0 +1,32 @@
+// for loop and while
+
+// print Hello world 10times
+
+function printHelloWorld() { 
+    for (let i = 1; i <= 10; i++){
+        console.log("Hello World");
+    }
+}
+printHelloWorld();
+
+// print hello world 
+function HelloWorld() {
+    for (let i = 3; i < 5; i++){
+        console.log("Hello World" + " " + i);
+    }
+}
+HelloWorld();
+
+function helloWorld(){
+    for(let i = 3; i <=5; i++){
+        console.log("Hello World " + " " + i);
+    }
+}
+helloWorld();
+
+function incLoopByTwo() { 
+    for (let i = 2; i < 9; i = i + 2) { 
+        console.log("Hello World By Increment By Two" + " "+ i) ;
+    }
+}
+incLoopByTwo();
