88b6bd71450ab52562d20256aa79413e27d603a7
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-12 16:38:12 +0530

    functions+loops

--- loops/loops.js
+++ loops/loops.js
@@ -79,3 +79,101 @@ for (let i = 0; i < arr.length; i++) {
         console.log("Value: " + " " + arr[i] + " is Odd");
     }
 }
+//Sum from 1 to N
+function sumToN(n) {
+  // your code
+  let sum = 0;
+  for(let i = 1; i <= n; i++){
+      sum = sum + i;
+  } 
+  console.log(sum);
+}
+sumToN(5);
+sumToN(15);
+sumToN(1);
+
+//Write a function that returns the number of digits in a positive integer.
+function countDigits(num){
+    let number = Math.abs(num).toString().length;
+    console.log(number);
+}
+countDigits(12345); // 5
+countDigits(7);     // 1
+countDigits(100);   // 3
+
+//Q6. Reverse a number
+function reverseNumber(num){
+    let reverse = 0;
+    while(num>0){
+        let digit = num%10;
+        //console.log(digit);
+        reverse = reverse*10+digit;
+        //console.log(reverse);
+        num=Math.floor(num/10);
+        //console.log(num);
+    }
+    console.log(reverse);
+}
+reverseNumber(12345); // 54321
+reverseNumber(1200);  // 21
+reverseNumber(987);   // 789
+
+//Functions + for loop
+function printNumbers(n) {
+    for(let i = 1; i<=n;i++){
+        console.log(i);
+    }
+}
+
+printNumbers(5);
+
+// print only even numbers
+function printEvenNumbers(n) {
+    for(let i = 1; i<=n; i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+}
+
+printEvenNumbers(10);
+
+//Find the sum of even numbers from 1 to N
+function sumEvenNumbers(n) {
+    let sum=0;
+    for(let i=1; i <=n; i++){
+        if (i%2==0) {
+            sum = sum+i;
+        }
+    }
+        return sum;
+}
+
+console.log(sumEvenNumbers(10)); // 30
+
+//Find the largest number from 1 to N
+function findLargest(n) {
+    let num = 0;
+
+    for(let i = 0; i<=n;i++){
+        if(i>num){
+            num=i;
+        }
+    }
+    return num;
+}
+
+console.log(findLargest(10)); // 10
+
+//Count numbers divisible by 3
+function countDivisibleBy3(n) {
+    let num = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            num = num+1;
+        }
+    }
+    return num;
+}
+
+console.log(countDivisibleBy3(10));
