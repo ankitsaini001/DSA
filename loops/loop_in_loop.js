056598218d1afe66697c7e2a48af5e15fbf912e2
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-16 23:40:53 +0530

    loop reverse practice

--- loops/loop_in_loop.js
+++ loops/loop_in_loop.js
@@ -11,7 +11,7 @@
     }
 }
 
-// llop with loop example
+// loop with loop example
 for (let i = 0; i < 5; i++) {
     for (let j = 0; j <= i; j++) {
         console.log("i = " + i + " j = " + j);
@@ -20,6 +20,13 @@ for (let i = 0; i < 5; i++) {
 
 for (let i = 0; i < 3; i++) {
     for (let j = i; j > 0; j--) {
+        console.log("i = " + i + " j = " + j);
+    }
+}
+
+// reverse looping
+for (let i = 5; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
         console.log("i = " + i + " j = " + j);
     }
 }
