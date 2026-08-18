8327b0408554ffa3bc98eea17912dddb63dc4848
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-18 18:06:01 +0530

    loop within loop

--- loops/loop_in_loop.js
+++ loops/loop_in_loop.js
@@ -30,3 +30,37 @@ for (let i = 5; i > 0; i--) {
         console.log("i = " + i + " j = " + j);
     }
 }
+
+function printPairs(n) {
+    for(let i=1; i<=n;i++){
+        for(let j=1;j<=n;j++){
+            console.log(i + " " + j);
+        }
+    }
+}
+
+printPairs(3);
+
+function countPairs(n) {
+    let count = 0;
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<=n; j++){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countPairs(3)); // 9
+console.log(countPairs(4)); // 16
+console.log(countPairs(5)); // 25
+
+let arr = [2, 5, 8];
+function printArrayPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i], arr[j]);
+        }
+    }
+}
+printArrayPairs(arr);
