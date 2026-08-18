760ae6187d56c72aae72d5aac1d5552a0cbde91f
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-18 23:22:03 +0530

    reverse pattern

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -32,3 +32,12 @@ for (let i = 0; i <= n; i++) {
     }
     console.log(row);
 }
+
+// reverse pattern
+for (let i = 0; i < n; i++){
+    let row = "";
+    for (let j = 0; j < n - i; j++) { 
+        row = row + (j + 1);
+    }
+    console.log(row);
+}
