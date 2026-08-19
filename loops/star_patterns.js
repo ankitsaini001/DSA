57934f0e050ccd59305037a3e588c1db52d6144d
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-19 23:28:32 +0530

    switch pattern

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -88,3 +88,17 @@ for (let i = 0; i < nstar; i++) {
     }
     console.log(row);
 }
+
+let toggle = 1;
+for (let i = 0; i < nstar; i++) { 
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row = row + toggle;
+        if (toggle === 1) {
+            toggle = 0;
+        } else { 
+            toggle = 1;
+        }   
+    }
+    console.log(row);
+}
