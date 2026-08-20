4cdd3291f34f8811970cac9b182c38ae3cb52f43
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-20 23:37:38 +0530

    Print a Reverse Right-Angled Triangle of Increasing Numbers

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -138,3 +138,12 @@ for (let i = 0; i < nxn; i++) {
     }
     console.log(row);
 }
+
+// Pattern 5: Print a Reverse Right-Angled Triangle of Increasing Numbers
+for(let i = 0; i<nxn;i++){
+    let row = "";
+    for(let j=0; j<nxn-i;j++){
+        row = row + (j+1);
+    }
+    console.log(row);
+}
