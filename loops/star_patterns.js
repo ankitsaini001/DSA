b620b66bf4d997db497dd787343cc6625aea4df6
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-20 23:32:57 +0530

    Print a Right-Angled Number Triangle

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -121,3 +121,20 @@ for(let i=0; i<nxn; i++){
     }
     console.log(row);
 }
+
+// Pattern 3: Print a Right-Angled Number Triangle
+for(let i=0; i< nxn; i++){
+    let row="";
+    for(let j=0; j<i; j++){
+        row = row + "*";
+    }
+    console.log(row);
+}
+
+for (let i = 0; i < nxn; i++) {
+    let row = "";
+    for (let j = 0; j < i; j++) {
+        row = row + (j + 1);
+    }
+    console.log(row);
+}
