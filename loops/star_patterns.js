17abd2dd7bbef4db5f61a9914db128fce413068e
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-21 23:53:52 +0530

    Print a Right-Aligned Right-Angled Triangle of Stars

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -159,3 +159,14 @@ for (let i = 0; i < nxn; i++) {
     }
     console.log(row);
 }
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  let toggle = 1;
+  for (let j = 0; j < i + 1; j++) {
+    row += toggle;
+    toggle = toggle === 1 ? 0 : 1;
+  }
+  console.log(row);
+}
