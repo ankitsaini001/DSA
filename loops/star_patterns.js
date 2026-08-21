6492ad433f8daecc4ba1d09439e20cb9333000e2
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-21 23:56:40 +0530

    loops practice

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -199,3 +199,12 @@ for (let i = 0; i < n; i++) {
   }
   console.log(row);
 }
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j <= i; j++) {
+    row += (j + 1);
+  }
+  console.log(row);
+}
