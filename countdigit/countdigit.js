43bd4bc2a0e5e1d63f6b17bc401bd308ce00fed6
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-24 23:32:45 +0530

    count digit corner case handle negative value

--- countdigit/countdigit.js
+++ countdigit/countdigit.js
@@ -40,3 +40,23 @@ console.log(countDigits(12));
 console.log(countDigits(1));
 console.log(countDigits(0));
 console.log(countDigits(-1));
+
+// handle negative number
+function countDigits(n) {
+    if (n === 0) {
+        return 1;
+    }
+    n = Math.abs(n); // it will change the negative value to positive number by which the count become 1 from 0.
+    let count = 0;
+    while (n > 0) {
+        n = Math.floor(n / 10);
+        count++;
+    }
+    return count;
+}
+console.log(countDigits(1233));
+console.log(countDigits(123));
+console.log(countDigits(12));
+console.log(countDigits(1));
+console.log(countDigits(0));
+console.log(countDigits(-1));
