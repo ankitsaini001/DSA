626f79380478aecc3657f07fd04182e35dac4eca
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-24 22:53:24 +0530

    count digit

--- countdigit/countdigit.js
+++ countdigit/countdigit.js
@@ -12,3 +12,12 @@ console.log(countDigit(1233));
 console.log(countDigit(123));
 console.log(countDigit(12));
 console.log(countDigit(1));
+
+// practice 1
+let n_count = 23;
+let count = 0;
+while (n_count > 0) {
+    n_count = Math.floor(n_count / 10);
+    count++;
+}
+console.log("count digit: " + count);
