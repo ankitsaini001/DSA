6a2dd75f192bb3bd1f8e888347b5e8a61b417347
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-26 00:20:40 +0530

    handle negative reverse number

--- Warm Up/palindrome/reverse.js
+++ Warm Up/palindrome/reverse.js
@@ -22,3 +22,20 @@ var reverse = function (x) {
 };
 console.log(reverse(121));
 console.log(reverse(123456789));
+
+// handle negative useDebugValue
+function reverseNegative(n) {
+    let nCopy = n;
+    let rev = 0;
+    n = Math.abs(n);
+    while (n > 0) {
+        let rem = n % 10;
+        rev = (10 * rev) + rem;   
+        n = Math.floor(n / 10);
+    }
+    return (nCopy < 0) ? -rev : rev;
+}
+
+console.log(reverseNegative(-111));  // expect -111
+console.log(reverseNegative(123));   // expect 321
+console.log(reverseNegative(-120));  // expect -21
