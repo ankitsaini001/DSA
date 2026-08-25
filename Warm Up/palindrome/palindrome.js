461e8686ca3c24f3164ca39f80a772be5eac0764
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-25 23:37:31 +0530

    palindrome handle return

--- Warm Up/palindrome/palindrome.js
+++ Warm Up/palindrome/palindrome.js
@@ -41,3 +41,26 @@ function checkNegativePalindrome(n) {
 console.log(checkNegativePalindrome(123));
 console.log(checkNegativePalindrome(121));
 console.log(checkNegativePalindrome(-121));
+
+// another better way to return the result
+function palindromeReturnCall(n){
+    if(n < 0){
+        return false;
+    }
+    let nCopy = n;
+    let rev = 0;
+
+    while(n > 0){
+        let rem = n % 10;
+        rev = (10 * rev) + rem;
+        n = Math.floor(n / 10);
+    }
+    // if(rev === nCopy){
+    //     return true;
+    // }else {
+    //     return false;
+    // }
+    return (rev === nCopy);
+}
+console.log(palindromeReturnCall(121));
+console.log(palindromeReturnCall(-121));
