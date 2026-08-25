9a8442d4238b137f1ac8a8142159c78533257c8d
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-25 23:54:10 +0530

    palindrome leetcode  exercise

--- Warm Up/palindrome/palindrome.js
+++ Warm Up/palindrome/palindrome.js
@@ -65,3 +65,30 @@ function palindromeReturnCall(n){
 console.log(palindromeReturnCall(121));
 console.log(palindromeReturnCall(-121));
 console.log(palindromeReturnCall(10));
+
+// leetcode
+var isPalindrome = function (x) {
+    // first lets check the negative value
+    if (x < 0) {
+        return false;
+    }
+    //copy the original value
+    let nCopy = x;
+    let reverse = 0;
+
+    // run the loop now
+    while (x > 0) {
+        let reminder = x % 10;
+        reverse = (10 * reverse) + reminder;
+        x = Math.floor(x / 10);
+    }
+    //    if(reverse === nCopy){
+    //     return true;
+    //    }else{
+    //     return false;
+    //    }
+    return (reverse === nCopy);
+};
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
