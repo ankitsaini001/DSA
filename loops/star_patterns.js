bc91de36a6c98f50c69ece11899b8100a87a575d
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-08-27 00:15:04 +0530

    printTriangle

--- loops/star_patterns.js
+++ loops/star_patterns.js
@@ -293,3 +293,15 @@ function printSquareTest(n) {
     }
 }
 printSquareTest(4);
+
+// practice 2, printTriangle
+function printTriangle(n){
+    for(let i=0; i<n; i++){
+        let row="";
+        for(let j=0; j<=i; j++){
+            row += "*";
+        }
+        console.log(row);
+    }
+}
+printTriangle(5);
