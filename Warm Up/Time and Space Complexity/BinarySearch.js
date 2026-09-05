bd525d3e81f91b3a50c8ab82cc856e120879d6c9
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-05 20:55:22 +0530

    Binary Search target 7

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -39,3 +39,22 @@ function binarySearchTarget2(num, target) {
 
 }
 console.log(binarySearchTarget2(num, 2));
+
+function binarySearchTarget7(num, target) { 
+    let left = 0;
+    let right = num.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (num[mid] == target) {
+            return mid;
+        } else if (num[mid] < target) {
+            left = mid + 1;
+        } else { 
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+console.log(binarySearchTarget7(num, 7));
