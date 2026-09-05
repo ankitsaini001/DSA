328a0bb77854a3c236f3a733340436c442faafc1
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-05 23:43:52 +0530

    Binary Search leetcode 704

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -58,3 +58,28 @@ function binarySearchTarget7(num, target) {
     return left;
 }
 console.log(binarySearchTarget7(num, 7));
+
+//Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+let nums = [-1, 0, 3, 5, 9, 12];
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
+console.log(search(nums, 9));
+console.log(search(nums, 2));
