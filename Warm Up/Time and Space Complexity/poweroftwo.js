eba71fb8c8bd5a36cfa882209f5da8f96f8c9e66
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-06 17:43:35 +0530

    leetcode: 231 power of two

---
+++ Warm Up/Time and Space Complexity/poweroftwo.js
@@ -0,0 +1,13 @@
+// leetcode: 231 power of two
+var isPowerOfTwo = function(n) {
+    if(n<=0){
+        return false;
+    }
+    while(n%2===0){
+        n = n/2;
+    }
+    return n===1;
+};
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(16));
+console.log(isPowerOfTwo(3));
