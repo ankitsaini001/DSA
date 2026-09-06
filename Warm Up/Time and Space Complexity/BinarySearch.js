0d8f9015fdb1af122e64d647c426e010d08884c8
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-06 13:41:43 +0530

    Leetcode: 35, search insert position

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -157,3 +157,25 @@ var searchBinary = function (nums, target) {
 }
 console.log(searchBinary([5, 7, 7, 8, 8, 10],8));
 console.log(searchBinary([5, 7, 7, 8, 8, 10],6));
+
+// Leetcode: 35, search insert position
+function searchInsert(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else { 
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
