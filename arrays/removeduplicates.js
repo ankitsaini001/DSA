cb75358f883d5918ecc676c1c2ae221e04e09490
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-08 23:24:26 +0530

    Array Non-decreasing remove duplicate practice_1

--- arrays/removeduplicates.js
+++ arrays/removeduplicates.js
@@ -136,3 +136,15 @@ var findDuplicate = function (nums) {
     return x + 1;
 }
 console.log(findDuplicate([0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,6,7,8,9,9,9]));
+
+function findDuplicateValue(nums) { 
+    let x = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[x]) { 
+            x = x + 1;
+            nums[x] = nums[i];
+        }
+    }
+    return x + 1;
+}
+console.log(findDuplicateValue([0,0,1,1,1,2,2,3,3,4]));
