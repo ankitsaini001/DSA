5a72ca3cf669b00440996e8d9ec8b2e82cc730a7
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-09 23:37:31 +0530

    binary search

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -266,3 +266,30 @@ function rotatedArray(nums) {
 }
 console.log(rotatedArray([4, 5, 6, 7, 0, 1, 2]));
 console.log(rotatedArray([3,4,5,1,2]));
+
+function searchSortedArray(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {  // fix 1: <= not 
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;  // fix 2: return immediately, don't just record
+        }
+        else if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target < nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+console.log(searchSortedArray([4,5,6,7,0,1,2],0));
