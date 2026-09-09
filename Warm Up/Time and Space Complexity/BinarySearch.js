dc5824e713555a563bf7508b1ed711d45e6d6264
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-09 22:50:37 +0530

    binary search

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -200,3 +200,51 @@ function searchInsert(nums, target) {
 console.log(searchInsert([1, 3, 5, 6], 5)); // 2 - found
 console.log(searchInsert([1, 3, 5, 6], 2)); // 1 - insert between 1 and 3
 console.log(searchInsert([1, 3, 5, 6], 7)); // 4 - insert at the end
+
+
+function findRightmost(nums, target) { 
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) { 
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] == target) {
+            result = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else { 
+            right = mid - 1;
+        }
+    }
+    return result;
+
+}
+
+function findLeftmost(nums, target) { 
+    let left = 0;
+    let right = nums.length - 1;
+    let result = -1;
+
+    while (left <= right) { 
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] == target) {
+            result = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else { 
+            right = mid - 1;
+        }
+    }
+    return result;
+}
+
+var search = function (nums, target) { 
+    let leftSearch = findLeftmost(nums, target);
+    let rightSearch = findRightmost(nums, target);
+    return [leftSearch, rightSearch];
+}
+console.log(search([5,7,7,8,8,10], 8));
