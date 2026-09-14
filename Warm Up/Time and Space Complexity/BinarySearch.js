186ce0aabf5f5e5165d2f43ee10cd8a71675ff92
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-14 23:49:32 +0530

    Binary search with Distinct integer

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -377,4 +377,24 @@ function sortTargetSearch(nums, target) {
 }
 console.log(sortTargetSearch(num, target));
 console.log(sortTargetSearch(num, 21));
+//Given a sorted array of distinct integers nums and a target value, return the index if target is found. If not, return the index where it would be if inserted in order, to keep the array sorted. Must run in O(log n).
+function distinctInteger(nums, target) { 
+    let left = 0;
+    let right = nums.length - 1;
 
+    while (left <= right) { 
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else { 
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+console.log(distinctInteger([1, 3, 4, 5, 6], 2));
+console.log(distinctInteger([1, 3, 4, 5, 6], 7));
+console.log(distinctInteger([1, 3, 4, 5, 6], -1));
+console.log(distinctInteger([1, 3, 4, 5, 6], 4));
