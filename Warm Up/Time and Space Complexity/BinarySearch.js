c2d333e664657e8b588b938bb6aee783ef0ddbde
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-17 23:05:22 +0530

    Search in Rotated Sorted Array (no duplicates)

--- Warm Up/Time and Space Complexity/BinarySearch.js
+++ Warm Up/Time and Space Complexity/BinarySearch.js
@@ -508,3 +508,27 @@ function rotatedArray(nums) {
 }
 console.log(rotatedArray([4,5,6,7,0,1,2]));
 console.log(rotatedArray([8,9,10,11,4,5,6,7]));
+
+//Search in Rotated Sorted Array (no duplicates)
+function searchRotated(nums, target) { 
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target < nums[mid]) {
+                right = mid + 1;
+            }
+        }
+    }
+    return -1;
+}
+console.log(searchRotated([4, 5, 6, 7, 0, 1, 2],0));
