f204e673bf185868e09056d28f9203b2d9b65a30
ankitsaini001 <17422563+ankitsaini001@users.noreply.github.com>
2026-09-26 19:48:16 +0530

    duplicate array

--- arrays/removeduplicates.js
+++ arrays/removeduplicates.js
@@ -150,3 +150,4 @@ function findDuplicateValue(nums) {
 console.log(findDuplicateValue([0,0,1,1,1,2,2,3,3,4]));
 console.log(findDuplicateValue([1,1,2]));
 console.log(findDuplicateValue([1, 1, 2, 3, 3, 5]));
+console.log(findDuplicateValue([1, 2, 3, 5, 3, 5]));
